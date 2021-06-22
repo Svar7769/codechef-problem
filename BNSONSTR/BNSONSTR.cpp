7c2d65075aa6c98457f640b826fb590ea3f11baa
Svar7769 <svarpatel7749@gmail.com>
2021-06-22 20:14:23 +0530

    BNSONSTR

--- BNSONSTR/BNSONSTR.cpp
+++ BNSONSTR/BNSONSTR.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main () {
     int t;
+    cin >> t;
     while (t--) {
         int n,sum=0;
         string s;
@@ -15,27 +16,28 @@ int main () {
             sum += s[i]-'0';
         }
 
+        int ans = n; // variable to store the final ans
+
         // checking for all value of d
         for (int d = 1; d <= n; d++) {
-            int ans = n; // variable to store the final ans
-            int ct=0; // variable to count solution
+            
             // check valid d
             if (n%d)continue;
 
             // check for all f's
             for (int f = 0; f<d ; f++) {
+                int ct = 0; // variable to count solution
                 // count no of one that are in position
-                for(int k=f; k<n; k+=f){
-                    if (s[k] == 1)
+                for(int k=f; k<n; k+=d){
+                    if (s[k] == '1')
                         ct += 1;
                     else 
                         ct--;
                 }
                 ans = min (ans, sum - ct);
             }
-            
-            // Printing ans
-            cout << ans << endl;
         }
+        // Printing ans
+        cout << ans << endl;
     }
 }
